6361e956730eec4e1cb0aa0970189f8b1e09ee95
seungheonlee <lsh2613@naver.com>
2025-04-19 14:57:46 +0900

    refactor: @RequestBody 활용하도록 수정

--- src/main/java/com/elasticsearch/PostDocumentController.java
+++ src/main/java/com/elasticsearch/PostDocumentController.java
@@ -1,5 +1,6 @@
 package com.elasticsearch;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -17,9 +19,8 @@ public class PostDocumentController {
     private final PostDocumentRepository postDocumentRepository;
 
     @PostMapping("/posts")
-    public ResponseEntity postDocument(@RequestParam String title,
-                                       @RequestParam String content) {
-        PostDocument postDocument = new PostDocument(title, content);
+    public ResponseEntity postDocument(@RequestBody PostCreateReq postCreateReq) {
+        PostDocument postDocument = new PostDocument(postCreateReq.getTitle(), postCreateReq.getContent());
         postDocumentRepository.save(postDocument);
         return ResponseEntity.ok(postDocument.getId());
     }
@@ -29,5 +30,11 @@ public class PostDocumentController {
                                  @PageableDefault Pageable pageable) {
         Page<PostDocument> postDocuments = postDocumentRepository.findByContentContaining(keyword, pageable);
         return ResponseEntity.ok(postDocuments);
+    }
+
+    @Data
+    public static class PostCreateReq {
+        private String title;
+        private String content;
     }
 }
